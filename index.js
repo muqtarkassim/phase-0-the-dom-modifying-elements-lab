43634ac7ba3ffcbd85099babb64541d77186959a
muqtar <muqtarabdiaziz8@gmail.com>
2023-09-16 14:17:17 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+const Main =document.querySelector('#main');
+Main.remove()
+const newHeader=document.createElement('h1');
+newHeader.textContent='YOUR-NAME is the champion';
+newHeader.id='victory'
